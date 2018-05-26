a580a5e48f064fa7095d4f10a71a2d1a2081f63b
Emily Welsch <emwelsch@gmail.com>
2018-05-26 22:40:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,24 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
 def initialize(name)
   @name = name
+  @songs = []
 end
 
-def add_song
+def add_song(song)
+    @songs << song
+  end
+
+def songs
+  @songs
 end
 
-def add_song_by_name
-end
+def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    @songs << song
+    song.artist = self
+  end
 
 def self.song_count
 end

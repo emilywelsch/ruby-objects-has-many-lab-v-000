3bdd048b14114363c45a469f922e25c0113c69e0
Emily Welsch <emwelsch@gmail.com>
2018-05-26 23:20:25 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,23 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << title
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if @author == nil
+      nil
+    else
+      @author.name
+    end
+  end
 
 end
+
